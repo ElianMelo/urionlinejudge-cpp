8ca4fbfa488ca98511f657b028f57eab68453013
ElianMelo <elianmelomorais@gmail.com>
2018-10-30 21:44:51 -0300

    1002 - Área do Círculo

---
+++ problemas/1002 - Área do Círculo.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    double pi, area, raio;
+
+    pi = 3.14159;
+
+    scanf("%lf", &raio);
+
+    area = pi * raio * raio;
+
+    printf("A=%.4lf\n", area);
+}
