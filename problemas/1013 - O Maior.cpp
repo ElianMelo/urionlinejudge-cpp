338ba3f3fb6eb1b2b13484dd074701d368740206
ElianMelo <elianmelomorais@gmail.com>
2018-12-18 10:24:32 -0200

    1013 - O Maior

---
+++ problemas/1013 - O Maior.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int A, B, C, maior;
+
+    scanf("%d %d %d", &A, &B, &C);
+
+    maior = (A + B + abs(A - B))/2;
+    maior = (maior + C + abs(maior - C))/2;
+
+    printf("%d eh o maior\n", maior);
+}
