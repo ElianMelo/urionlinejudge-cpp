f8065e8b2e6d10bcbeb11fc2a2067f4178b14d0c
ElianMelo <elianmelomorais@gmail.com>
2018-12-18 10:16:48 -0200

    1006 - Média 2

---
+++ problemas/1006 - Média 2.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    float A, B, C, MEDIA;
+
+    scanf("%f %f %f", &A, &B, &C);
+
+    MEDIA = ((A * 2) + (B * 3) + (C * 5)) / 10;
+
+    printf("MEDIA = %.1f\n", MEDIA);
+}
