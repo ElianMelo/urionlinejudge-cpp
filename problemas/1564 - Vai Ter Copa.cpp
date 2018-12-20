fc2614ad6501cd9e31db97e4b7d12311ef654675
ElianMelo <elianmelomorais@gmail.com>
2018-12-20 21:47:15 -0200

    1564 - Vai Ter Copa?

---
+++ problemas/1564 - Vai Ter Copa.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int reclam = 0;
+
+    while(scanf("%d", &reclam) != EOF){
+        if(reclam > 0){
+            printf("vai ter duas!\n");
+        }else if(reclam == 0){
+            printf("vai ter copa!\n");
+        }
+    }
+}
