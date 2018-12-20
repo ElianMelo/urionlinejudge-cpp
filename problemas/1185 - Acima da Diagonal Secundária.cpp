232342e9c1f920c5f2ce4e336dcf5dd2858711c1
ElianMelo <elianmelomorais@gmail.com>
2018-12-20 21:40:44 -0200

    1185 - Acima da Diagonal Secundária

---
+++ problemas/1185 - Acima da Diagonal Secundária.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    char op;
+    double m[12][12], limite = 11, cont = 0;
+    double soma = 0, media;
+
+    cin >> op;
+
+    for(int i = 0; i < 12; i++){
+        for(int j = 0; j < 12; j++){
+            cin >> m[i][j];
+            if(j < limite){
+                soma += m[i][j];
+                cont++;
+            }
+        }
+        limite--;
+    }
+
+    media = soma / cont;
+
+    if(op == 'S'){
+        printf("%.1lf\n", soma);
+    }else if(op == 'M'){
+        printf("%.1lf\n", media);
+    }
+
+}
