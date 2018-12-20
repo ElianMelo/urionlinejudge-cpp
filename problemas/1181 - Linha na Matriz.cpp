4e99fcd54e1c6bcf8af9807aa4452e1b2217c09b
ElianMelo <elianmelomorais@gmail.com>
2018-12-20 21:31:46 -0200

    1181 - Linha na Matriz

---
+++ problemas/1181 - Linha na Matriz.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int num, i, j;
+    char op;
+    double mat[12][12], soma = 0;
+
+    scanf("%d", &num);
+    getchar();
+    scanf("%c", &op);
+
+    for(i = 0; i < 12; i++){
+        for(j = 0; j < 12; j++){
+            scanf("%lf", &mat[i][j]);
+            if(num == i){
+                soma += mat[i][j];
+            }
+        }
+    }
+
+    if(op == 'S'){
+        printf("%.1lf\n", soma);
+    }else{
+        printf("%.1lf\n", soma/12);
+    }
+
+}
