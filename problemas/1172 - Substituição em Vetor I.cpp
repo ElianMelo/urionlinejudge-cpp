6dd2489b93f9e0a8c405c4b60272980c52914c05
ElianMelo <elianmelomorais@gmail.com>
2018-12-20 21:19:14 -0200

    1172 - Substituição em Vetor I

---
+++ problemas/1172 - Substituição em Vetor I.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int x[10], i;
+
+    for(i = 0; i < 10; i++){
+        scanf("%d", &x[i]);
+
+        if(x[i] <= 0){
+            x[i] = 1;
+        }
+        printf("X[%d] = %d\n", i, x[i]);
+    }
+
+}
