986cafbdab57f37d837dc1a1021d49785701c510
ElianMelo <elianmelomorais@gmail.com>
2018-12-20 21:06:12 -0200

    1144 - Sequência Lógica

---
+++ problemas/1144 - Sequência Lógica.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int quant;
+
+    cin >> quant;
+
+    for(long long int i = 1; i <= quant; i++){
+        printf("%lld %lld %lld\n", i, i*i, i*i*i);
+        printf("%lld %lld %lld\n", i, i*i+1, i*i*i+1);
+    }
+
+}
